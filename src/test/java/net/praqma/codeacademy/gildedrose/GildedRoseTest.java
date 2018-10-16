bee6e0c9256f7c88433c1310a69269312c49ce1e
Sofus Albertsen <soa@praqma.net>
2018-10-16 11:53:17 +0200

    fixed a typo

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -42,7 +42,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("Another Thing", 0, 8) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("Should degrade with one",6,app.items[0].quality );
+        assertEquals("Should degrade with one",5,app.items[0].quality );
         
     }
 }
