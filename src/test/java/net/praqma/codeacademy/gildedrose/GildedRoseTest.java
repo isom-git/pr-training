29ddfdb1e71f33c5c9ac92f85870c22b7708b269
Majid Hasnain <majid.hasnain2@ikea.com>
2018-10-25 14:34:18 +0200

    aaa

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -8,7 +8,7 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 1) };
+        Item[] items = new Item[] { new Item("foo", 0, 11) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
