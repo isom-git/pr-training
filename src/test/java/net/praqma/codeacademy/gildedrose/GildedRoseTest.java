a6950e5440d8e80e30cd73cbea41f2fe93e1e00d
Julius <julius.aberg@ikea.com>
2018-10-25 14:32:22 +0200

    test comment added

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
-
+//test comment
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
